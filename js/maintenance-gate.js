296d42963479e684a7230b0d42a4346174be71d2
w shadow <muhdrahbar39@gmail.com>
2026-08-27 15:05:22 +0530

    Delete js directory
    Signed-off-by: w shadow <muhdrahbar39@gmail.com>

--- js/maintenance-gate.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  if (typeof MAINTENANCE_MODE === "undefined" || !MAINTENANCE_MODE) return;
-
-  if (typeof hasValidStaffSession === "function" && hasValidStaffSession()) {
-    return;
-  }
-
-  window.location.replace("maintenance.html");
-})();
